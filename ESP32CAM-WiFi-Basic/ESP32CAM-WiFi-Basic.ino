b5373641650692fce7d4692ee7dc91ded05eac2b
RogerVazquezNieves <rogelio.vn@sjuanrio.tecnm.mx>
2022-05-13 16:31:43 -0500

    cambiar ssid

--- ESP32CAM-WiFi-Basic/ESP32CAM-WiFi-Basic.ino
+++ ESP32CAM-WiFi-Basic/ESP32CAM-WiFi-Basic.ino
@@ -21,8 +21,8 @@
 #include <WiFi.h>  // Biblioteca para manejar el WiFi del ESP32CAM
 
 // Datos de Red
-const char* ssid = "********";  // Pon aquí el nombre de la red a la que deseas conectarte
-const char* password = "********";  // Escribe la contraseña de dicha red
+const char* ssid = "Totalplay-DFA0";  // Pon aquí el nombre de la red a la que deseas conectarte
+const char* password = "DFA051FD9NC6vk6n";  // Escribe la contraseña de dicha red
 
 // Objetos
 WiFiClient espClient; // Este objeto maneja las variables necesarias para una conexion WiFi
